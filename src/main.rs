d56d45649c1e10542721a42ffc7b8775e3026cf4
John Boyd <johnboydiv@gmail.com>
2017-10-02 21:32:25 -0500

    Port to RTFM v0.2 (#4)
    * Selected MIT license
    
    * Remove examples
    
    * Create basic RTFM v0.2 template
    
    Used the following blog post as a guide:
    http://blog.japaric.io/rtfm-v2/

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,32 @@
-#![feature(const_fn)]
-#![feature(used)]
+#![feature(proc_macro)] // <- IMPORTANT! Feature gate for procedural macros
+#![feature(const_unsafe_cell_new)]
 #![no_std]
 
-// version = "0.2.9"
-#[macro_use]
-extern crate cortex_m;
-
-// version = "0.2.3"
-#[allow(unused_extern_crates)]
-extern crate cortex_m_rt;
-
-// version = "0.1.0"
-#[macro_use]
-extern crate cortex_m_rtfm as rtfm;
-
-// version = "0.4.1"
 extern crate f3;
+extern crate cortex_m_rtfm as rtfm; // <- this rename is required
+extern crate cortex_m_semihosting as semihosting;
 
-use f3::stm32f30x;
-use rtfm::{P0, P1, T0, T1, TMax};
-use stm32f30x::interrupt::Exti0;
+use core::fmt::Write;
+use rtfm::app; // <- this is a procedural macro
+use semihosting::hio;
 
-fn init(_prio: P0, _thr: &TMax) {
-    // Set breakpoint at beginning of init() to allow debugger to easily
-    // step into beginning of application code using RTFM framework.
-    rtfm::bkpt();
+// This macro expands into the `main` function
+app! {
+    // this is a path to a _device_ crate, a crate generated using svd2rust
+    device: f3::stm32f30x,
 }
 
-fn idle(_prio: P0, _thr: T0) -> ! {
-    rtfm::request(t1);
+// INITIALIZATION
+fn init(_p: init::Peripherals) {
+    // Nothing to initialize in this example ...
+}
 
-    // Sleep
+// IDLE LOOP
+fn idle() -> ! {
+    writeln!(hio::hstdout().unwrap(), "Hello, world!").unwrap();
+
+    // Go to sleep
     loop {
         rtfm::wfi();
     }
-}
-
-tasks!(stm32f30x, {
-    t1: Task {
-        interrupt: Exti0,
-        priority: P1,
-        enabled: true,
-    },
-});
-
-fn t1(_task: Exti0, _prio: P1, _thr: T1) {
-    rtfm::bkpt();
-
-    hprintln!("Hello, world!");
-
-    rtfm::bkpt();
-}
+} 
