2b39fb96617cec3f9d7b356800aa3a40380630bf
phreaknik <johnboydiv@gmail.com>
2017-10-22 13:31:38 -0500

    Copy code from blinky example

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,42 @@
-#![feature(proc_macro)] // <- IMPORTANT! Feature gate for procedural macros
-#![feature(const_unsafe_cell_new)]
+#![deny(unsafe_code)]
+#![deny(warnings)]
+#![feature(proc_macro)]
 #![no_std]
 
-extern crate cortex_m_rtfm as rtfm;
 extern crate f3;
+extern crate cortex_m;
+extern crate cortex_m_rtfm as rtfm;
 
-use f3::prelude::*;
-use f3::Serial;
-use f3::serial::Event;
-use f3::time::Hertz;
+use cortex_m::peripheral::SystClkSource;
+use f3::led::{self, LEDS};
 use rtfm::{app, Threshold};
 
-const BAUD_RATE: Hertz = Hertz(115_200);
+const FREQUENCY: u32 = 4; // Hz
 
 // TASKS & RESOURCES
 app! {
     device: f3::stm32f30x,
 
+    resources: {
+        static ON: bool = false;
+    },
+
     tasks: {
-        USART1_EXTI25: {
-            path: loopback,
-            resources: [USART1],
+        SYS_TICK: {
+            path: toggle,
+            resources: [ON],
         },
     }
 }
 
 // INITIALIZATION
-fn init(p: init::Peripherals) {
-    let serial = Serial(p.USART1);
+fn init(p: init::Peripherals, _r: init::Resources) {
+    led::init(p.GPIOE, p.RCC);
 
-    serial.init(BAUD_RATE.invert(), Some(p.DMA1), p.GPIOA, p.RCC);
-    serial.listen(Event::Rxne);
+    p.SYST.set_clock_source(SystClkSource::Core);
+    p.SYST.set_reload(8_000_000 / FREQUENCY);
+    p.SYST.enable_interrupt();
+    p.SYST.enable_counter();
 }
 
 // IDLE LOOP
@@ -42,13 +48,13 @@ fn idle() -> ! {
 }
 
 // TASKS
-// Send back the received byte
-fn loopback(_t: &mut Threshold, r: USART1_EXTI25::Resources) {
-    let serial = Serial(&**r.USART1);
+// Toggle the state of the LED
+fn toggle(_t: &mut Threshold, r: SYS_TICK::Resources) {
+    **r.ON = !**r.ON;
 
-    // Grab the byte we just received
-    let byte = serial.read().unwrap();
-
-    // and send it back
-    serial.write(byte).unwrap();
+    if **r.ON {
+        LEDS[0].on();
+    } else {
+        LEDS[0].off();
+    }
 }
